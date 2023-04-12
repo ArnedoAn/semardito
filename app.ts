89f15c7836d902a17b7f6c4d1b72606a80473b06
Andrés Arnedo Escalante <79600570+ArnedoAn@users.noreply.github.com>
2023-04-12 23:29:42 +0000

    Function paradign

--- app.ts
+++ app.ts
@@ -8,8 +8,9 @@ import * as xlsx from "xlsx";
 import fs from "fs";
 import Excel from "exceljs";
 
-import indexRouter from "./routes/index";
-import usersRouter from "./routes/users";
+import indexRouter from "./routes/contabilidad";
+import usersRouter from "./routes/asistencia";
+import { isNumberObject } from "util/types";
 
 const app = express();
 
@@ -47,101 +48,81 @@ const bot = new TelegramBot("6052206060:AAH2E79emRRoZFEVlT3QNx3KWKA4Kzncq6o", {
 });
 
 // Ubicación del archivo de hoja de cálculo
-const archivoHojaDeCalculo = path.join(
+const archivoContabilidad = path.join(
   __dirname,
   "documents",
   "contabilidad.xlsx"
 );
 
+const archivoAsistencia = path.join(__dirname, "documents", "asistencia.xlsx");
+
 // Verificar si el archivo existe
-if (!fs.existsSync(archivoHojaDeCalculo)) {
-  // Si no existe, crearlo
-  const workbook = new Excel.Workbook();
-  const sheet = workbook.addWorksheet("Sheet1");
-  // Configurar las columnas y encabezados de la hoja de cálculo
-  sheet.columns = [
-    { header: "Cantidad", key: "cantidad" },
-    { header: "Descripción", key: "descripcion" },
-    { header: "Fecha", key: "fecha" },
-  ];
-  workbook.xlsx
-    .writeFile(archivoHojaDeCalculo)
-    .then(() => {
-      console.log("Archivo de hoja de cálculo creado exitosamente");
-      // Continuar con la lógica de manejo de mensajes de Telegram
-    })
-    .catch((err: any) => {
-      console.error("Error al crear archivo de hoja de cálculo:", err);
-    });
-} else {
-  // Continuar con la lógica de manejo de mensajes de Telegram
-
-  // Manejador para el comando /in
-  bot.onText(/\/in/, async (msg) => {
-    const chatId = msg.chat.id;
-    const messageId = msg.message_id;
-    //const userId = msg.from.id;
-
-    // Enviar mensaje solicitando la cantidad
-    bot.sendMessage(chatId, "Por favor, ingrese la cantidad monetaria:");
-
-    // Escuchar el siguiente mensaje del usuario
-    bot.once("message", (msg) => {
-      const cantidad = msg.text;
-      console.log(cantidad);
-
-      // Enviar mensaje solicitando la descripción
-      bot.sendMessage(chatId, "Por favor, ingrese la descripción:");
-
-      // Escuchar el siguiente mensaje del usuario
-      bot.once("message", (msg) => {
-        const descripcion = msg.text;
-        console.log(descripcion);
-
-        // Obtener la fecha actual
-        const fecha = new Date().toLocaleDateString();
-
-        // Crear objeto con los datos del registro
-        const registro = {
-          cantidad,
-          descripcion,
-          fecha,
-        };
-
-        // Leer la hoja de cálculo existente o crear una nueva si no existe
-        let workbook;
-        if (fs.existsSync(archivoHojaDeCalculo)) {
-          workbook = xlsx.readFile(archivoHojaDeCalculo);
-        } else {
-          workbook = xlsx.utils.book_new();
-        }
-
-        const sheetName = "Sheet1";
-        const sheet = workbook.Sheets[sheetName] || "";
-
-        // Obtener el rango de celdas ocupadas en la hoja de cálculo
-        const range = xlsx.utils.decode_range(sheet["!ref"]!);
-        const newRow = range.e.r + 1;
-
-        // Escribir los datos del registro en la hoja de cálculo
-        sheet[`A${newRow}`] = { t: "n", v: parseFloat(registro.cantidad!) };
-        sheet[`B${newRow}`] = { t: "s", v: registro.descripcion };
-        sheet[`C${newRow}`] = { t: "s", v: registro.fecha };
-
-        // Guardar la hoja de cálculo actualizada
-        xlsx.writeFile(workbook, archivoHojaDeCalculo);
-
-        // Registrar el mensaje en la consola del servidor
-        console.log(
-          `Usuario registró: Cantidad: ${registro.cantidad}, Descripción: ${registro.descripcion}, Fecha: ${registro.fecha}`
-        );
-
-        // Enviar un mensaje de confirmación al usuario
-        bot.sendMessage(chatId, "Registro agregado exitosamente.");
+const veryfySheet = async (archivoHojaDeCalculo: any) => {
+  if (!fs.existsSync(archivoHojaDeCalculo)) {
+    // Si no existe, crearlo
+    const workbook = new Excel.Workbook();
+    const sheet = workbook.addWorksheet("Sheet1");
+    // Configurar las columnas y encabezados de la hoja de cálculo
+    sheet.columns = [
+      { header: "Cantidad", key: "cantidad" },
+      { header: "Descripción", key: "descripcion" },
+      { header: "Fecha", key: "fecha" },
+    ];
+    workbook.xlsx
+      .writeFile(archivoHojaDeCalculo)
+      .then(() => {
+        console.log("Archivo de hoja de cálculo creado exitosamente");
+        // Continuar con la lógica de manejo de mensajes de Telegram
+      })
+      .catch((err: any) => {
+        console.error("Error al crear archivo de hoja de cálculo:", err);
       });
-    });
-  });
-}
+  } else {
+    console.log("Archivo de hoja de cálculo encontrado");
+  }
+};
+
+bot.onText(/\/start/, (msg) => {
+  const chatId = msg.chat.id;
+  const messageId = msg.message_id;
+
+  bot.sendMessage(
+    chatId,
+    "Bienvenido al bot de contabilidad. Por favor, ingrese el comando /in para registrar una entrada o el comando /out para registrar una salida."
+  );
+});
+
+bot.onText(/\/out/, (msg) => {
+  const chatId = msg.chat.id;
+  const messageId = msg.message_id;
+
+  bot.sendMessage(chatId, "Por favor, ingrese la cantidad monetaria:");
+});
+
+async ({ cantidad, descripcion, sheetPath }: any) => {
+  const fecha = new Date().toLocaleDateString();
+  const workbook = xlsx.readFile(sheetPath);
+
+  const sheetName = "Sheet1";
+  const sheet = workbook.Sheets[sheetName] || "";
+
+  // Obtener el rango de celdas ocupadas en la hoja de cálculo
+  const range = xlsx.utils.decode_range(sheet["!ref"]!);
+  const newRow = range.e.r + 1;
+
+  // Escribir los datos del registro en la hoja de cálculo
+  sheet[`A${newRow}`] = { t: "n", v: parseFloat(cantidad!) };
+  sheet[`B${newRow}`] = { t: "s", v: descripcion };
+  sheet[`C${newRow}`] = { t: "s", v: fecha };
+
+  // Guardar la hoja de cálculo actualizada
+  xlsx.writeFile(workbook, sheetPath);
+
+  // Registrar el mensaje en la consola del servidor
+  console.log(
+    `Usuario registró: Cantidad: ${cantidad}, Descripción: ${descripcion}, Fecha: ${fecha}`
+  );
+};
 
 app.listen(3000, () => {
   console.log("Server started on port 3000");
